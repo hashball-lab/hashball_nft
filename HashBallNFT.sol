1ee8a3cdb102935c1a1592d174148e55c107e8f7
hashball-lab <polisjob@proton.me>
2025-03-13 09:41:43 +0800

    Create HashBallNFT.sol

---
+++ HashBallNFT.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/common/ERC2981.sol";
+
